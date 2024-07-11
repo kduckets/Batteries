571964a31984cb4fe95ea382ed0e878a15f5a1f4
kduckets <kmditroia@gmail.com>
2024-07-11 11:53:11 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 "use client"
-import Battery from "@/components/Battery";
+import Battery from "../components/Battery";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+    <main className="flex min-h-screen flex-col items-center justify-between ">
+      <div className="z-10 w-full items-center justify-between font-mono text-sm lg:flex">
        <Battery/>
       </div>
     </main>
